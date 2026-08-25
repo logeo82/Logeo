b22e4326379d3e28450adef5d46fba57696d23e1
logeo82 <frison.mario12@gmail.com>
2026-08-26 01:34:26 +0200

    Enable ChercherTrouver import module

--- boot.py
+++ boot.py
@@ -6,8 +6,9 @@ import owner_extended
 import seloger_import_v2
 try:
     import market_search
+    import market_import
 except Exception as e:
-    print('LOGEO market search disabled:', e)
+    print('LOGEO market search/import disabled:', e)
 from waitress import serve
 
 serve(app, host='0.0.0.0', port=int(os.environ.get('PORT','5000')))
