e8662971ec31e7295305e28b26e5a66ee5a78d42
logeo82 <frison.mario12@gmail.com>
2026-08-25 23:53:12 +0200

    Load extended owner listing module at startup

--- boot.py
+++ boot.py
@@ -2,6 +2,7 @@ import os
 from app import app
 import owner_import
 import advanced_import
+import owner_extended
 from waitress import serve
 
 serve(app, host='0.0.0.0', port=int(os.environ.get('PORT','5000')))
